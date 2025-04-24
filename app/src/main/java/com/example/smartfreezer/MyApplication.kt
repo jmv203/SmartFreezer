a8023f93d3e478bdc15c627afe1cf4557a9ff62a
Jesus <jesusiko653@gmail.com>
2025-04-24 19:18:40 +0200

    intentado solucionar la api

---
+++ app/src/main/java/com/example/smartfreezer/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.smartfreezer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyAplicattion : Application() {
+}
---
+++ app/src/main/java/com/example/smartfreezer/viewmodels/RecipesViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.smartfreezer.viewmodels
+
+class RecipeViewModel {
+}
