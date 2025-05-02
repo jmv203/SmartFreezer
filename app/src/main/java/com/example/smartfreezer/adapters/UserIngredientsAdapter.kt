f6bb33c8cfec88542b8d5539edaa9ab367e4e471
Jesus <jesusiko653@gmail.com>
2025-05-02 15:07:28 +0200

    Detalles de receta integrado

---
+++ app/src/main/java/com/example/smartfreezer/adapters/UserIngredientsAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.smartfreezer.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smartfreezer.databinding.ItemIngredientBinding
+import com.example.smartfreezer.models.UserIngredient
+
+class IngredientsUserAdapter : RecyclerView.Adapter<IngredientsUserAdapter.IngredientViewHolder>() {
+
+    private var userIngredientList = listOf<UserIngredient>()
+    private val selectedIngredientIcons = mutableSetOf<String>() // Store icons
+
+    // Modifica la función setIngredients para eliminar duplicados basados en el nombre
+    fun setIngredients(ingredients: List<UserIngredient>, preselectedIcons: List<String> = emptyList()) {
+        val uniqueIngredients = ingredients
+            .groupBy { it.name }
+            .map { it.value.first() }
+
+        userIngredientList = uniqueIngredients
+        selectedIngredientIcons.clear()
+        selectedIngredientIcons.addAll(preselectedIcons)
+        notifyDataSetChanged()
+    }
+
+    fun getSelectedIngredientIcons(): List<String> { // Return icons
+        return selectedIngredientIcons.toList()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        val binding = ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return IngredientViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        val ingredient = userIngredientList[position]
+        holder.bind(ingredient)
+    }
+
+    override fun getItemCount(): Int {
+        return userIngredientList.size
+    }
+
+    inner class IngredientViewHolder(private val binding: ItemIngredientBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(ingredient: UserIngredient) {
+            binding.ingredientCheckBox.text = ingredient.name
+            binding.ingredientCheckBox.isChecked = selectedIngredientIcons.contains(ingredient.icon)
+
+            binding.ingredientCheckBox.setOnCheckedChangeListener(null)
+            binding.ingredientCheckBox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    selectedIngredientIcons.add(ingredient.icon)
+                } else {
+                    selectedIngredientIcons.remove(ingredient.icon)
+                }
+            }
+        }
+    }
+}
